63db28ca08e348d081f7a13adab9b3cd4089861f
he1gi <oleh.mishchenko1@gmail.com>
2022-08-11 18:47:37 +0300

    added task 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+  startBtnEl: document.querySelector('[data-start]'),
+  stopBtnEl: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+
+function onStartBtnClick() {
+  refs.startBtnEl.setAttribute('disabled', 'disabled');
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  refs.startBtnEl.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startBtnEl.addEventListener('click', onStartBtnClick);
+refs.stopBtnEl.addEventListener('click', onStopBtnClick);
